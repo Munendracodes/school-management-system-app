ae538fad39ab55af9c137dad7c6de4c59846922e
Suman <gundapanthulasuman@gmail.com>
2026-05-24 12:40:29 +0530

    Updated web build v1

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f48b3d2dffc86aa505bc163feaf25293
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "749562e3775faa9b83d27485bff230db",
+"flutter_bootstrap.js": "393f37069aec4862d8fb8ebaf1ae957a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

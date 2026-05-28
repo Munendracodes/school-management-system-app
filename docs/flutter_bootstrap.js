92fa1493076a3214109ebb0b363471b85a47d479
Suman <gundapanthulasuman@gmail.com>
2026-05-28 17:37:36 +0530

    web changes

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2380461126"
+    serviceWorkerVersion: "241732298"
   }
 });
